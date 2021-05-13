1daeb4acd49c3ced14ec7af15f30d662bfa1d4d7
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-05-14 01:21:07 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     private static int N, C;
-    private static final String path = "example.txt";
-    private static Location<Integer>[] loc;
+    private static final String path = "D:\\_a_Lecture Notes FSKTM\\_Semester 2\\_WIA1002_DataStructures\\Projects\\src\\example.txt";
+    private static final Map map = new Map();
 
     public static void main(String[] args) {
         readInputFile();
-        for (Location<Integer> location : loc) {
-            System.out.println(location.toString());
-        }
+        map.printMap();
     }
 
     public static void readInputFile() {
@@ -20,14 +18,14 @@ public class Main {
             Scanner inText = new Scanner(new FileInputStream(path));
 
             String[] firstLine = inText.nextLine().split(" ");
-            N = Integer.parseInt(firstLine[0]);
+            N = Integer.parseInt(firstLine[0]); //N represents the number of locations in the text file
             C = Integer.parseInt(firstLine[1]);
-            loc = (Location<Integer>[]) new Location[N]; //N represents the number of locations in the text file
 
             for (int i = 0; i < N; i++) {
                 String[] line_i = inText.nextLine().split(" ");
-                loc[i] = new Location<Integer>(Double.parseDouble(line_i[0]), Double.parseDouble(line_i[1]), Integer.parseInt(line_i[2]), i);
+                map.addLoc(Double.parseDouble(line_i[0]), Double.parseDouble(line_i[1]), Integer.parseInt(line_i[2]), i);
             }
+            map.completeConnect();
             inText.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
