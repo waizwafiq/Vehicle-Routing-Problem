a5d4d00c48d51288b621019c6974629dd3480015
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-05-14 00:25:32 +0800

    Generified Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     private static int N, C;
-    private static final String path = "D:\\_a_Lecture Notes FSKTM\\_Semester 2\\_WIA1002_DataStructures\\Projects\\src\\example.txt";
-    private static Location[] loc;
+    private static final String path = "example.txt";
+    private static Location<Integer>[] loc;
 
     public static void main(String[] args) {
         readInputFile();
-        for (Location location : loc) {
+        for (Location<Integer> location : loc) {
             System.out.println(location.toString());
         }
     }
@@ -22,11 +22,11 @@ public class Main {
             String[] firstLine = inText.nextLine().split(" ");
             N = Integer.parseInt(firstLine[0]);
             C = Integer.parseInt(firstLine[1]);
-            loc = new Location[N]; //N represents the number of locations in the text file
+            loc = (Location<Integer>[]) new Location[N]; //N represents the number of locations in the text file
 
             for (int i = 0; i < N; i++) {
                 String[] line_i = inText.nextLine().split(" ");
-                loc[i] = new Location(Double.parseDouble(line_i[0]), Double.parseDouble(line_i[1]), Integer.parseInt(line_i[2]), i);
+                loc[i] = new Location<Integer>(Double.parseDouble(line_i[0]), Double.parseDouble(line_i[1]), Integer.parseInt(line_i[2]), i);
             }
             inText.close();
         } catch (FileNotFoundException e) {
