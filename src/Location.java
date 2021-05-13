65bb897e4a090a2db0bf24962442b216fec4b7a1
Waiz Wafiq <63910911+waizwafiq@users.noreply.github.com>
2021-05-13 23:51:41 +0800

    Update Location.java

--- src/Location.java
+++ src/Location.java
@@ -1,20 +1,25 @@
 public class Location {
-    private double x_coord, y_coord;
+    //the Location class refers to the vertices in the graph
+    //note: the graph is complete, weighted (euclidean distance) and undirected.
+    private double x, y;
     private int demand_size, ID;
+    private boolean visited;
 
-    public Location(double x_coord, double y_coord, int demand_size, int ID) {
-        this.x_coord = x_coord;
-        this.y_coord = y_coord;
+    public Location(double x, double y, int demand_size, int ID) {
+        this.visited = false;
+        this.x = x;
+        this.y = y;
         this.demand_size = demand_size;
         this.ID = ID;
     }
 
-    public double getX_coord() {
-        return x_coord;
+    //GETTERS
+    public double getX() {
+        return x;
     }
 
-    public double getY_coord() {
-        return y_coord;
+    public double getY() {
+        return y;
     }
 
     public int getDemand_size() {
@@ -25,11 +30,42 @@ public class Location {
         return ID;
     }
 
+    public boolean isVisited() {
+        return visited;
+    }
+
+    //SETTERS
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setDemand(int demand_size) {
+        this.demand_size = demand_size;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Location visit() {
+        this.visited = true;
+        return this;
+    }
+
+    public Location unvisit() {
+        this.visited = false;
+        return this;
+    }
+
     @Override
     public String toString() {
         return "Location{" +
-                "x_coord=" + x_coord +
-                ", y_coord=" + y_coord +
+                "x=" + x +
+                ", y=" + y +
                 ", demand_size=" + demand_size +
                 ", ID=" + ID +
                 '}';
