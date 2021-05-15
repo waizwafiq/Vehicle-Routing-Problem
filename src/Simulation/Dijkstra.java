7b155ac18f2c7b73ae3ffbb9848f9809df4ade7b
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-15 16:52:33 +0800

    Updated comments

--- src/Simulation/Dijkstra.java
+++ src/Simulation/Dijkstra.java
@@ -20,31 +20,39 @@ public class Dijkstra {
 
     private static void search(Vertex start, Vertex end) {
         /*
-        PSEUDOCODE:
-            min = +infinity
-            distV = {0, +inf, +inf, ...} <- for every vertices
-            minEdge = null <-- the edge with the lowest dist connected from the vertex
-            Start from the depot (ID 0)
+        PSEUDOCODE?:
+            Given a graph, G.
+            While all vertices aren't visited:
+                Set dT as 0. (dT is the total distance travelled)
 
-            Let currV: current Vertex
-            Loop through all vertices other than its own vertex:
-                Let currE: current Edge
-                Loop through all edges connected from currV:
-                    if the destination hasn't been visited yet:
-                        min = Math.min(min, dist)
+                Let dV: the expected path each vertex take
+                Set dV = {+inf, +inf, +inf, ...}
 
+                Send a vehicle.
+                Start from the depot (ID 0)
 
+                Let currV: current Vertex
+                Loop through all other vertices:
+                    Let currE: current Edge
+                    Loop through all edges/path connected from currV:
+                        if the demand is sufficient AND the destination isn't visited AND dT + the path's distance < dV:
+                            choose this path.
+                            update dV with the new expected total distance value
+                            
+                    Add the chosen vertex to go into the "visited" list
+                    Update the total distance travelled by the vehicle.
+                    Deduct the capacity (send the package)
+                    Go to the chosen vertex
         */
-
-        double min = 0;
-
-        //array of shortest dist selected by each vertex, (initially +inf to get the minimum)
+        //array of expected dist selected by each vertex, (initially +inf to get the minimum)
         double[] distV = new double[G.size()];
         ArrayList<Integer> visitedID = new ArrayList<>(); //a list of visited vertices (based on ID) except depot
 
         while (visitedID.size() != distV.length - 1) {
             //while all vertices haven't been visited
             //EACH LOOP REPRESENTS ONE DELIVERY VEHICLE
+
+            double dT = 0; //the total distance travelled
             Arrays.fill(distV, Double.POSITIVE_INFINITY);
 
             Vertex currentVertex = G.getHead();
@@ -58,25 +66,29 @@ public class Dijkstra {
                     //go through every edges connected to current vertex
                     Edge currentEdge = currentVertex.EdgeList.get(j); //starting from the first edge
 
-                    if (tempC >= currentEdge.destination.capacity && min + currentEdge.dist < distV[i] && !visitedID.contains(currentEdge.destination.ID)) {
-                        /* IF (capacity >= demand) AND (min + dist < chosen_path_dist) AND (the destination hasn't been visited yet):
+                    if (tempC >= currentEdge.destination.capacity && dT + currentEdge.dist < distV[i] && !visitedID.contains(currentEdge.destination.ID)) {
+                        /* IF (capacity >= demand) AND (dT + dist < expected_path_dist) AND (the destination hasn't been visited yet):
                                 choose this path.
-                         */
-                        nextVertex = currentEdge.destination;
-                        distV[i] = min + currentEdge.dist;
+                        */
+                        nextVertex = currentEdge.destination; // path to go
+                        distV[i] = dT + currentEdge.dist;  //update the path value the vertex holds
                     }
                 }
-                visitedID.add(nextVertex.ID);
+                visitedID.add(nextVertex.ID); //the nextVertex has been visited.
                 System.out.print(" --> " + nextVertex);
-                min = distV[i];
-                tempC -= nextVertex.capacity;
+
+                //update the values
+                dT = distV[i]; //update total distance travelled
+                tempC -= nextVertex.capacity; //deduct capacity
                 currentVertex = nextVertex;
 
                 if (currentVertex.ID == 0)
+                    //if the vehicle returns to the depot, break the loop/go to the next vehicle
                     break;
             }
-            visitedID.remove((Integer) 0); //<-- make it as an object
+            visitedID.remove((Integer) 0); //used Integer to make it as an object
             System.out.println();
+            System.out.println("dT : "+dT);
         }
     }
 }
