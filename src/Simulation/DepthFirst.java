62fe56306801235e79820649a473d3abe0193266
zulfathihanafi <fathiimran7@gmail.com>
2021-05-17 17:04:33 +0800

    Tree updated with the right path

--- src/Simulation/DepthFirst.java
+++ src/Simulation/DepthFirst.java
@@ -3,9 +3,8 @@ package Simulation;
 import GraphComponent.Edge;
 import GraphComponent.Vertex;
 import map.Graph;
+import map.Tree;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DepthFirst {
@@ -13,6 +12,7 @@ public class DepthFirst {
     private static Graph G;
     private static int C;
     private static double tourCost;
+    private static List<List<Integer>> tree;
 
     public static void run(Graph G, int C) {
         tourCost = 0;
@@ -25,71 +25,27 @@ public class DepthFirst {
         System.out.println("Tour Cost: " + tourCost);
         //System.out.println(result);
         System.out.println("Execution time: " + (double) (end - start) * Math.pow(10, -6) + "ms\n");
-        int[] vertexID = new int[G.size()-1];
 
-        for(int i=1;i<G.size();i++){
-            vertexID[i-1] = i;
-        }
-        List<List<Integer>> result1 = new DepthFirst().permute(vertexID);
-        result1 = removeDuplicates(result1);
-        System.out.println("\nPossible permutations of the said array:");
-        result1.forEach(System.out::println);
+       // tree = Tree.getTree();
 
 
     }
 
+    private static void search() {
 
-    //permutation of all integer(ID)
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Permutation(0, nums, result);
-        return result;
-    }
-    private void Permutation(int i, int[] nums, List<List<Integer>> result) {
-        if (i == nums.length - 1) {
-            List<Integer> list = new ArrayList<>();
-            int currentCapacity = 0;
-            for (int j=0;j< nums.length;j++){
-                int ID = nums[j]; // will store the ID
-                currentCapacity+= G.getVertex(ID).capacity;
-                if(currentCapacity<C) list.add(ID);
-                else{
-                    currentCapacity-=G.getVertex(ID).capacity;
+        for (int i = 0; i < tree.size(); i++) {//tree contains list of list, we get the list here
+            Vertex currentVertex = G.getHead();
+            Vertex nextVertex = G.getHead();
+            for (int j = 0; j < tree.get(i).size(); j++) { //getting the integer(ID)
+                int ID = tree.get(i).get(j); //possible path for route
+                for(int k=0;k<currentVertex.EdgeList.size();k++){
+                    Edge currentEdge = currentVertex.EdgeList.get(k);
+                    if(currentEdge.destination.ID==ID){
+
+                    }
                 }
             }
-            result.add(list);
-
-        } else {
-            for (int j = i, l = nums.length; j < l; j++) {
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                Permutation(i + 1, nums, result);
-                temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-            }
         }
-    }
-    public static List<List<Integer>> removeDuplicates(List<List<Integer>> list)
-    {
-
-        // Create a new ArrayList
-        List<List<Integer>> newList = new ArrayList<>();
-
-        // Traverse through the first list
-        for (List<Integer> element : list) {
-
-            // If this element is not present in newList
-            // then add it
-            if (!newList.contains(element)) {
-
-                newList.add(element);
-            }
-        }
-
-        // return the new list
-        return newList;
     }
 
 }
--- src/map/Tree.java
+++ src/map/Tree.java
@@ -1,7 +1,5 @@
 package map;
 
-import Simulation.DepthFirst;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,27 +8,46 @@ public class Tree {
     private static int C;
     private static double tourCost;
     private static List<List<Integer>> tree;
-
+//permutation
     public static void run(Graph G, int C) {
+        /*
+    PSEUDOCODE :
+        - Find all possible combination for the number ID , example : 1 2 3
+        will become  1, 2, 3, 1 2, 1 3 , 1 2 3
+
+        - Find all combination :
+        1,2,3,1 2, 2 1, 1 3, 3 1....etc
+
+
+
+
+
+
+    */
 
         Tree.G = G;
         Tree.C = C;
         System.out.println("---Generating tree---\n");
+        tree = new ArrayList<>();
 
 
         int[] vertexID = new int[G.size()-1];
         for(int i=1;i<G.size();i++){
-            vertexID[i-1] = i;
+            vertexID[i-1] = i;  // All ID in the tree
         }
-        tree = new Tree().permute(vertexID);
-        tree = removeDuplicates(tree); //remove any duplicate list
+
+        //this will compute the possible combination
+        Integer[][] answer ;
+        for(int i=1;i<=4;i++){
+            answer = nCrArray(vertexID,i);
+            for(int j=0;j<answer.length;j++){
+                tree.addAll(permute(answer[j])); // the generated arrays of combination will undergoes permutation process
+            }
+        }
+        //permutation
+        tree = removeDuplicates(tree); //remove any duplicate list, and add 0 to the first and last node
         System.out.println("\nPossible tree : ");
         tree.forEach(System.out::println);
-
-
-
-
-
     }
 
     public static List<List<Integer>> getTree() {
@@ -38,22 +55,25 @@ public class Tree {
     }
 
     //permutation of all integer(ID)
-    public List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permute(Integer[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Permutation(0, nums, result);
         return result;
     }
-    private void Permutation(int i, int[] nums, List<List<Integer>> result) {
+    private static void Permutation(int i, Integer[] nums, List<List<Integer>> result) {
         if (i == nums.length - 1) {
             List<Integer> list = new ArrayList<>();
             int currentCapacity = 0;
             for (int j=0;j< nums.length;j++){
                 int ID = nums[j]; // will store the ID
                 currentCapacity+= G.getVertex(ID).capacity;
-                if(currentCapacity<C) list.add(ID);
+                if(currentCapacity<=C){
+                    list.add(ID);
+                }
                 else{
                     currentCapacity-=G.getVertex(ID).capacity;
                 }
+
             }
             result.add(list);
 
@@ -69,15 +89,15 @@ public class Tree {
             }
         }
     }
-    public static List<List<Integer>> removeDuplicates(List<List<Integer>> list)
-    {
+    public static List<List<Integer>> removeDuplicates(List<List<Integer>> list) {
 
         // Create a new ArrayList
         List<List<Integer>> newList = new ArrayList<>();
 
         // Traverse through the first list
         for (List<Integer> element : list) {
-
+            element.add(0,0);
+            element.add(0); // add 0 at first and last node, to set it as a route/path
             // If this element is not present in newList
             // then add it
             if (!newList.contains(element)) {
@@ -88,5 +108,53 @@ public class Tree {
 
         // return the new list
         return newList;
+    }
+
+    //combination
+    public static int factorial(int num) {
+        int fact = 1;
+        for (int i = 1; i <= num; i++)
+            fact = fact*i;
+        return fact;
+    }
+    // Get the nCr
+    public static int nCr(int n, int r) {
+        return factorial(n)/(factorial(n-r)*factorial(r));
+    }
+    // Get the combinations of the array
+    public static Integer[][] nCrArray(int[] array, int r) {
+        int n = array.length;
+        // Choose r from the length of the array
+        int ncr = nCr(n,r);
+        Integer[][] result = new Integer[ncr][r];
+        int result_index = 0;
+        // BASE CASE
+        if (r == 1) {
+            // Each value in the array is a result
+            for (; result_index < array.length; result_index++)
+                result[result_index][0] = array[result_index];
+        }
+        // All other cases
+        else {
+            // Iterate through each of the starting values
+            for (int i = 0; i < array.length-r+1; i++) {
+                // Create the sub-array
+                int[] recursivearray = new int[n-i-1];
+                // Copy into the sub-array all values after the active value
+                System.arraycopy(array, i+1, recursivearray, 0, n-i-1);
+                // Calculate the sub-combinations (recurse)
+                Integer[][] subarrays = nCrArray(recursivearray, r-1);
+                // Create the results for this active value
+                for (Integer[] subarray : subarrays) {
+                    // Augment the active value and sub-combinations
+                    result[result_index][0] = array[i];
+                    System.arraycopy(subarray, 0,
+                            result[result_index], 1,
+                            subarray.length);
+                    result_index++;
+                }
+            }
+        }
+        return result;
     }
 }
