eacfdf2a272f6020fe51d571014551ad3824a92f
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-05-31 19:25:28 +0800

    Added MCTS-NRPA algorithm

---
+++ src/Simulation/MCTS/NRPA.java
@@ -0,0 +1,30 @@
+package Simulation.MCTS;
+
+import map.Graph;
+
+import java.util.Arrays;
+
+public class NRPA {
+    private static Graph G;
+    private static int N, C;
+    private static double tourCost;
+
+    private static int[][][] policy;
+    private static int level = 3;
+
+    public static void run(Graph G, int N, int C) {
+        NRPA.G = G;
+        NRPA.N = N;
+        NRPA.C = C;
+        NRPA.policy = new int[level][N][N];
+
+        //FILL EACH ROW OF THE FIRST LEVEL WITH 0's
+        for (int[][] tubes : policy)
+            Arrays.stream(tubes).forEach(row -> Arrays.fill(row, 0));
+
+
+
+
+    }
+
+}
