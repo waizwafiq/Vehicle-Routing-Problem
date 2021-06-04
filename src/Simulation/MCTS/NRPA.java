9f25ca29fbf065a1999f1821c1add94dbc4dd104
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-04 21:54:42 +0800

    Updated MCTS-NRPA

--- src/Simulation/MCTS/NRPA.java
+++ src/Simulation/MCTS/NRPA.java
@@ -15,16 +15,34 @@ public class NRPA {
         NRPA.G = G;
         NRPA.N = N;
         NRPA.C = C;
-        int level = 3;
+        int level = 3, iterations = 100, ALPHA = 1;
+
         NRPA.policy = new int[level][N][N];
 
         //FILL EACH ROW OF THE FIRST LEVEL WITH 0's
         for (int[][] tubes : policy)
             Arrays.stream(tubes).forEach(row -> Arrays.fill(row, 0));
+    }
 
+    private static int search(int level, int iterations) {
 
+        if (level == 0)
+            return rollout();
 
+        int[][] globalPolicy = policy[level];
 
+        for (int i = 0; i < iterations; i++){
+
+        }
+        return 0;
+    }
+
+    private static void adapt() {
+
+    }
+
+    private static int rollout() {
+        return 0;
     }
 
 }
