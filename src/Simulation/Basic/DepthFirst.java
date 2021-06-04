95e0948b7e1581615fa1e71c33ad5668af0d83ad
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-04 23:03:55 +0800

    Made a few changes

--- src/Simulation/Basic/DepthFirst.java
+++ src/Simulation/Basic/DepthFirst.java
@@ -3,7 +3,6 @@ package Simulation.Basic;
 import GraphComponent.Edge;
 import GraphComponent.Vertex;
 import map.Graph;
-import map.Tree;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,15 +10,14 @@ import java.util.List;
 
 public class DepthFirst {
     //the id is array
-    private Graph G;
-    private int C;
-    private double tourCost;
-    private List<String> tree;
+    private static Graph G;
+    private static int C;
+    private static List<String> tree;
 
-    public void run(Graph G, int C) {
-        tourCost = 0;
-        this.G = G;
-        this.C = C;
+    public static void run(Graph G, int C) {
+        double tourCost = 0;
+        DepthFirst.G = G;
+        DepthFirst.C = C;
         System.out.println("---DF Search---\n");
         long start = System.nanoTime();
 //        String result = search(G.getHead());
@@ -30,31 +28,28 @@ public class DepthFirst {
         tree = new ArrayList<>();
         search(0, 0, "");
 
-        Iterator itr = tree.iterator();
-
-        while(itr.hasNext()){
-            Object element = itr.next();
+        for (Object element : tree) {
             System.out.println(element);
         }
 
     }
 
-    private void search(int capacity, int vertexID,String currentList) {
-       Vertex currentVertex = G.getVertex(vertexID);
-       capacity +=currentVertex.capacity;
-       if(vertexID!=0){
-           currentList += vertexID+" ";
-       }
-       for(int i=0;i<currentVertex.EdgeList.size();i++){
-           Edge currentEdge = currentVertex.EdgeList.get(i);
-           Vertex destination = currentEdge.destination;
-           if(capacity+destination.capacity>C || currentList.contains(destination.ID + " ") || destination.ID==0){
-               continue;
-           }else{
-              // System.out.println(capacity);
-               search(capacity, destination.ID, currentList);
-           }
-       }
+    private static void search(int capacity, int vertexID, String currentList) {
+        Vertex currentVertex = G.getVertex(vertexID);
+        capacity += currentVertex.capacity;
+        if (vertexID != 0) {
+            currentList += vertexID + " ";
+        }
+        for (int i = 0; i < currentVertex.EdgeList.size(); i++) {
+            Edge currentEdge = currentVertex.EdgeList.get(i);
+            Vertex destination = currentEdge.destination;
+            if (capacity + destination.capacity > C || currentList.contains(destination.ID + " ") || destination.ID == 0) {
+                //put something here?
+            } else {
+                // System.out.println(capacity);
+                search(capacity, destination.ID, currentList);
+            }
+        }
         tree.add(currentList);
 
     }
