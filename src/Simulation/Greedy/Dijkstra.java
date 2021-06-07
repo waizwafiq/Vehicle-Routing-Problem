b2dbb932d2384e5720c1ab10202a083b0282997d
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-07 14:20:17 +0800

    Implemented heterogeneous vehicle capacity feature

--- src/Simulation/Greedy/Dijkstra.java
+++ src/Simulation/Greedy/Dijkstra.java
@@ -10,10 +10,12 @@ import java.util.Arrays;
 public class Dijkstra {
 
     private static Map G;
-    private static int C;
+    private static int C, lorries;
     private static double tourCost;
 
-    public static void run(Map G, int C) {
+    public static void run(Map G, int C, int numberOfLorries) {
+        Dijkstra.lorries = numberOfLorries;
+
         tourCost = 0; //just in case if we want to do multiple Dijkstra searches (to reset)
         Dijkstra.G = G;
         Dijkstra.C = C;
@@ -57,19 +59,29 @@ public class Dijkstra {
         ArrayList<Integer> visitedID = new ArrayList<>(); //a list of visited vertices (based on ID) except depot
         StringBuilder outString = new StringBuilder();
 
-        int vehicleCount = 0;
+        int vehicleCount = 0, lorryCount = 0;
         while (visitedID.size() != distV.length - 1) {
+            //EACH LOOP REPRESENTS ONE DELIVERY VEHICLE
             //while all vertices haven't been visited
-            outString.append("Vehicle ").append(++vehicleCount).append("\n"); //EACH LOOP REPRESENTS ONE DELIVERY VEHICLE
+
+            int tempC;
+            if (lorries != 0) {
+                tempC = 2 * C; //to deduct the capacity in vehicle whenever a vertex is visited
+                outString.append("Vehicle ").append(++vehicleCount).append(" (Lorry ").append(++lorryCount).append(")\n");
+                lorries--;
+            } else {
+                tempC = C;
+                outString.append("Vehicle ").append(++vehicleCount).append("\n");
+            }
 
             double dT = 0; //the total distance travelled
             Arrays.fill(distV, Double.POSITIVE_INFINITY);
 
             Vertex currentVertex = G.getHead();
             Vertex nextVertex = G.getHead();
-            int tempC = C; //to deduct the capacity in vehicle whenever a vertex is visited
 
             outString.append(currentVertex);
+            int totalCapacity = 0;
             for (int i = 0; i < G.size(); i++) {
                 //go through every vertices in the graph
                 for (int j = 0; j < currentVertex.EdgeList.size(); j++) {
@@ -90,6 +102,7 @@ public class Dijkstra {
                 //update the values
                 dT = distV[i]; //update total distance travelled
                 tempC -= nextVertex.capacity; //deduct capacity
+                totalCapacity += nextVertex.capacity;
                 currentVertex = nextVertex;
 
                 if (currentVertex.ID == 0)
@@ -97,7 +110,7 @@ public class Dijkstra {
                     break;
             }
             visitedID.remove((Integer) 0); //used Integer to make it as an object
-            outString.append("\nCapacity: ").append(C - tempC);
+            outString.append("\nCapacity: ").append(totalCapacity);
             outString.append("\nCost: ").append(dT).append("\n");
             tourCost += dT;
         }
