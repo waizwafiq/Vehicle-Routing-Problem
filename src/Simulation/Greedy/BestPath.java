d7397ec97be3d8965deafbf312762e5f609e3081
Waiz Wafiq <mwaizwafiq@gmail.com>
2021-06-12 03:57:46 +0800

    Made some debugging

--- src/Simulation/Greedy/BestPath.java
+++ src/Simulation/Greedy/BestPath.java
@@ -6,8 +6,10 @@ import map.Map;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class BestPath {
+
     private static Map G;
     private static int C, lorries;
     private static double tourCost = 0;
@@ -46,11 +48,18 @@ public class BestPath {
                             update cV with the new expected total cost value
                             keep the chosen path's demand size to update dTs
                     Add the chosen vertex to go into the "visited" list
-                    Update the total distance travelled by the vehicle.
+                    Update the total distance travelled by the vehicle. (dT = dV - chosen_demand)
                     Deduct the capacity (send the package)
                     Go to the chosen vertex using the chosen path.
         */
         //array of expected edge cost selected by each vertex, (initially +inf to get the minimum)
+
+        for (int i = 0; i < G.size(); i++) {
+            ArrayList<Edge> e = G.getVertex(i).EdgeList;
+            Collections.sort(e);
+        }
+
+
         double[] costV = new double[G.size()];
         ArrayList<Integer> visitedID = new ArrayList<>(); //a list of visited vertices (based on ID) except depot
         StringBuilder outString = new StringBuilder();
@@ -59,14 +68,11 @@ public class BestPath {
         while (visitedID.size() != costV.length - 1) {
             //while all vertices haven't been visited
             int tempC;
-            boolean lorryUsed = false;
-            outString.append("---------------------\n");
             if (lorries != 0) {
                 //if there are still lorries to be dispatched out:
                 tempC = 2 * C; //to deduct the capacity in lorry whenever a vertex is visited
                 outString.append("Vehicle ").append(++vehicleCount).append(" (Lorry ").append(++lorryCount).append(")\n");
                 lorries--;
-                lorryUsed = true;
             } else {
                 tempC = C;
                 outString.append("Vehicle ").append(++vehicleCount).append("\n");
@@ -79,45 +85,33 @@ public class BestPath {
 
             outString.append(currentVertex);
             int totalCap = 0;
-            boolean dispatched = false;
             for (int i = 0; i < G.size(); i++) {
                 //go through every vertices in the graph
 
-
+                int tempCap = 0; //holds temp capacity to subtract at dT
                 for (int j = 0; j < currentVertex.EdgeList.size(); j++) {
                     //go through every edges connected to current vertex
                     Edge currentEdge = currentVertex.EdgeList.get(j); //starting from the first edge
 
-                    if (lorryUsed && currentEdge.destination.narrowArea)
-                        //if lorry is currently used and the destination is in the narrow area, don't go here
-                        continue;
-
-                    if (tempC >= currentEdge.destination.capacity && currentEdge.dist < costV[i] && !visitedID.contains(currentEdge.destination.ID)) {
-                        /* IF (capacity >= demand) AND (dist < expected_path_dist) AND (the destination hasn't been visited yet):
+                    if (tempC >= currentEdge.destination.capacity && dT + currentEdge.dist < costV[i] && !visitedID.contains(currentEdge.destination.ID)) {
+                        /* IF (capacity >= demand) AND (dT + dist < expected_path_dist) AND (the destination hasn't been visited yet):
                                 choose this path.
                         */
-                        dispatched = true;
                         nextVertex = currentEdge.destination; // path to go
-                        costV[i] = currentEdge.dist;  //update the path cost value the vertex holds
-
+                        costV[i] = dT + currentEdge.dist;  //update the path cost value the vertex holds
                     }
                 }
-
-                if (!dispatched && nextVertex.ID == 0) {
-                    outString.append(" --X-> NOT DISPATCHED");
-                    break;
-                }
-
                 visitedID.add(nextVertex.ID); //the nextVertex has been visited.
                 outString.append(" --> ").append(nextVertex);
 
                 //update the values
-                dT += costV[i]; //update total distance travelled
+                dT = costV[i]; //update total distance travelled
                 tempC -= nextVertex.capacity; //deduct capacity
                 totalCap += nextVertex.capacity;
                 currentVertex = nextVertex;
 
-                if (nextVertex.ID == 0)
+                if (currentVertex.ID == 0)
+                    //if the vehicle returns to the depot, break the loop/go to the next vehicle
                     break;
             }
             visitedID.remove((Integer) 0); //used Integer to make it as an object
